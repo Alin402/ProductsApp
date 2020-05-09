189ee85bd48d2c3415a392967716a2656b72417a
Alin402 <balanescu.alin03@gmail.com>
2020-05-09 16:02:27 +0300

    Updates

---
+++ precache-manifest.cf5893c0920901a6c6e459b005cf9830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "295f117adb6f19c65e17e7bcff9dc6f8",
+    "url": "/ProductsApp/index.html"
+  },
+  {
+    "revision": "7e06ce0b0112e40010b5",
+    "url": "/ProductsApp/static/css/main.1bef387e.chunk.css"
+  },
+  {
+    "revision": "d45317105e956e0db588",
+    "url": "/ProductsApp/static/js/2.80644eeb.chunk.js"
+  },
+  {
+    "revision": "3e169c07641d86ad1b71e13ffe5a346f",
+    "url": "/ProductsApp/static/js/2.80644eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e06ce0b0112e40010b5",
+    "url": "/ProductsApp/static/js/main.421d19b2.chunk.js"
+  },
+  {
+    "revision": "7980dc228e0cf66ac50a",
+    "url": "/ProductsApp/static/js/runtime-main.5d6b8eba.js"
+  }
+]);
